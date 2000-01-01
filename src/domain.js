119c772e61da82af2136d463ef07292fc613aef5
Pairwise baseline migration <pairwise-baseline@localhost>
2000-01-01 00:00:00 +0000

    A/B baseline migrated from bca6d4c7ad700f19e81d72ca4b9b4ac4d49c8cf2

---
+++ src/domain.js
@@ -0,0 +1,4 @@
+export const absenceStates = ['reported', 'inviting', 'covered', 'uncovered', 'cancelled'];
+export const invitationStates = ['pending', 'accepted', 'declined', 'expired', 'invalidated'];
+export const skipReasons = ['unqualified', 'schedule-conflict', 'workload-limit', 'already-held'];
+export const assignmentSources = ['ranked', 'manual'];
